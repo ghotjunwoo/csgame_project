60dd16e48c6f1bc38e664af6364c51903792f300
soondae126 <73982555+soondae126@users.noreply.github.com>
2020-11-05 17:09:19 +0900

    ^^ (#1)
    * ^^
    
    * k

--- main.py
+++ main.py
@@ -144,7 +144,17 @@ while True:
         if t == 255:
             t = 0
             ult_time = 0
-
+        ult1 = pg.draw.rect(screen, (0, 255, 255 - t), (0.5 * system.width, 0.5 * system.height, 70, 350))
+        ult2 = pg.draw.rect( screen, (0, 255, 255 - t), (0.5 * system.width-150, 0.5 * system.height+150, 350, 70))
+        t += 5
+        if t >= 200 and ult1.colliderect(player.get_rect()) or ult1.colliderect(player.get_rect()):
+            player.health -= 220
+            t = 0
+            ult_time = 0
+        if t == 255:
+            t = 0
+            ult_time = 0
     # 화면 갱신
     player.movetohead()
     pg.display.flip()
+
